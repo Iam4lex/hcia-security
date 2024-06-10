6a1db018502202e7f3c62c86acb2f7f95027d5ff
[alex-mwangi] <[mungaialexm@gmai.com]>
2024-06-10 07:53:55 +0300

    first commit

---
+++ network-securit-concepts.txt.sh
@@ -0,0 +1,5 @@
+During data communication, varous insecure factors may cause information breach incomplteness and unavailability inquring negative impact.
+
+Network security refers to cyber security. Which is cyber space security at international level.
+
+
